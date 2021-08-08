72de3a7a8e41f428d2f76a84bcdaaf0c67bbe8e4
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-08-08 13:20:25 +0600

    structure sidebar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Content from "./components/layout/Content";
 import Header from "./components/layout/Header";
 
 function App() {
   return (
     <div>
       <Header/>
+      <Content/>
     </div>
   );
 }
---
+++ src/components/layout/Sidebar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FaChevronDown, FaInbox, FaRegCalendar, FaRegCalendarAlt } from 'react-icons/fa';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li>
+          <span>
+            <FaInbox/>
+          </span>
+          <span>
+            Inbox
+          </span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar/>
+          </span>
+          <span>
+            Today
+          </span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt/>
+          </span>
+          <span>
+            Next 7 days
+          </span>
+        </li>
+      </ul>
+
+      <div className="sidebar__middle">
+        <span><FaChevronDown/></span>
+        <h2>Projects</h2>
+      </div>
+
+      <ul className="sidebar__projects">Projects will be here</ul>
+
+      Add Project Component Here!
+    </div>
+  )
+}
+
+export default Sidebar;
